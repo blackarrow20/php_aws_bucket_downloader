fe39a9a448f361b26668b47341ac69a3ebee84e2
adajic <adajic@awesomemotive.com>
2023-05-05 22:17:27 +0200

    Empty storageFolder means we are referring to the "whole bucket".

--- s3config_example.php
+++ s3config_example.php
@@ -10,6 +10,6 @@ $endpoint = ""; // For Amazon S3 Direct leave as empty string!
                 // For example for BackBlaze we use: "https://s3.us-west-004.backblazeb2.com"
 $directory = "C:/xampp/htdocs/AWS_downloader/downloaded"; // Local folder, where to download bucket files?
 $bucket = "duplicator-s3-test"; // Amazon s3 bucket
-$storageFolder = ""; // Don't start with slash '/', for example: "Duplicator Backups/test/"
+$storageFolder = ""; // Don't start with slash '/', for example: "Duplicator Backups/test/". Empty means whole bucket!
 $region = "us-east-1"; // This will work for our Amazon S3 test account
 $version = "2006-03-01"; // Keep this unchanged unless you know what you are doing
