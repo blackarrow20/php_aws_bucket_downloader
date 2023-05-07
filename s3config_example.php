43e2fb539559b39f9081e73250cdeff0e5ec1c3b
adajic <adajic@awesomemotive.com>
2023-05-07 12:45:20 +0200

    Added new things to gitignore, fixed path in s3config_example.php

--- s3config_example.php
+++ s3config_example.php
@@ -8,7 +8,7 @@ $s3secret = "";
 $endpoint = ""; // For Amazon S3 Direct leave as empty string!
                 // For example we use for Google Cloud: "https://storage.googleapis.com"
                 // For example for BackBlaze we use: "https://s3.us-west-004.backblazeb2.com"
-$directory = "C:/xampp/htdocs/AWS_downloader/downloaded"; // Local folder, where to download bucket files?
+$directory = "C:/xampp/htdocs/php_aws_bucket_downloader/downloaded"; // Local folder, where to download bucket files?
 $bucket = "duplicator-s3-test"; // Amazon s3 bucket
 $storageFolder = ""; // Don't start with slash '/', for example: "Duplicator Backups/test/". Empty means whole bucket!
 $region = "us-east-1"; // This will work for our Amazon S3 test account
